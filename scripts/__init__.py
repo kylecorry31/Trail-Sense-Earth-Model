4544585e7139b7e55c4669b00d070e9e921cae31
Kyle Corry <kylecorry31@gmail.com>
2023-07-15 11:46:30 -0400

    Improve compressed resolution

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -13,6 +13,29 @@ def load_pixels(path, resize=None):
     im = load(path, resize)
     return np.array(im)
 
+def compress_to_webp2(paths, output_filename, map_point=lambda x: x, a=1, b=0, invalid_value=-999, quality=100, lossless=False, resize_source=None):
+    if '/' in output_filename and not os.path.exists(output_filename.rsplit('/', 1)[0]):
+        os.makedirs(output_filename.rsplit('/', 1)[0])
+    images = [load(path, resize_source) for path in paths]
+    new_im = Image.new('RGB' if len(images) == 3 else 'L', (images[0].size[0], images[0].size[1]), color='black')
+    pixels = new_im.load()
+
+    for x in range(new_im.size[0]):
+        for y in range(new_im.size[1]):
+            ts = [im.getpixel((x, y)) for im in images]
+            if invalid_value in ts:
+                if len(images) == 1:
+                    pixels[x, y] = 0
+                else:
+                    pixels[x, y] = (0, 0, 0)
+            else:
+                mapped = [int(a * (map_point(t) + b)) for t in ts]
+                if len(images) == 1:
+                    pixels[x, y] = mapped[0]
+                else:
+                    pixels[x, y] = (mapped[0], mapped[1], mapped[2])
+    new_im.save(output_filename, quality=quality, lossless=lossless, format='WEBP')
+
 def compress_to_webp(paths, output_filename, map_point=lambda x: x, offset=0, invalid_value=-999, quality=100, lossless=False, resize_source=None):
     if '/' in output_filename and not os.path.exists(output_filename.rsplit('/', 1)[0]):
         os.makedirs(output_filename.rsplit('/', 1)[0])
--- scripts/compression.py
+++ scripts/compression.py
@@ -1,20 +1,27 @@
-from . import get_min_max, compress_to_webp
+from . import get_min_max, compress_to_webp2, compress_to_webp
 from .progress import progress
 from PIL import Image
 
+# Pixel = a * value + b
+# Value = (pixel - b) / a
+
 def minify_multiple(files, map_point, invalid_value, data_point, use_rgb, quality, lossless, size=None):
     Image.MAX_IMAGE_PIXELS = None
-    with progress('Calculating compression offset', 1) as pbar:
+    with progress(f'Calculating compression offset ({data_point})', 1) as pbar:
         offset = get_min_max(files, map_point, invalid_value, size)
         pbar.update(1)
 
-    print(f'Offset ({data_point}): {-offset[0]} for {data_point} (Min: {offset[0]}, Max: {offset[1]}, New Min: 0, New Max: {min(offset[1] - offset[0], 255)})')
+    b = -offset[0]
+    # Map to 0-255 after subtracting the offset
+    a = 255 / (offset[1] + b)
+
+    print(f'A: {a}, B: {b}')
 
     grouping = 3 if use_rgb else 1
     
-    with progress('Compressing', len(files) // grouping) as pbar:
+    with progress(f'Compressing ({data_point})', len(files) // grouping) as pbar:
         for i in range(0, len(files), grouping):
-            compress_to_webp(files[i:i+grouping], f'output/{data_point}-{i + 1}-{i + grouping}.webp', map_point, offset[0], invalid_value, quality, lossless, size)
+            compress_to_webp2(files[i:i+grouping], f'output/{data_point}-{i + 1}-{i + grouping}.webp', map_point, a, b, invalid_value, quality, lossless, size)
             pbar.update(grouping)
 
 def minify(path, map_point, invalid_value, output_filename, quality, lossless, size=None):
@@ -23,8 +30,12 @@ def minify(path, map_point, invalid_value, output_filename, quality, lossless, s
         offset = get_min_max([path], map_point, invalid_value)
         pbar.update(1)
 
-    print(f'Offset: {-offset[0]} (Min: {offset[0]}, Max: {offset[1]}, New Min: 0, New Max: {min(offset[1] - offset[0], 255)})')
+    b = -offset[0]
+    # Map to 0-255 after subtracting the offset
+    a = 255 / (offset[1] + b)
+
+    print(f'A: {a}, B: {b}')
     
     with progress('Compressing', 1) as pbar:
-        compress_to_webp([path], output_filename, map_point, offset[0], invalid_value, quality, lossless, size)
+        compress_to_webp2([path], output_filename, map_point, a, b, invalid_value, quality, lossless, size)
         pbar.update(1)
