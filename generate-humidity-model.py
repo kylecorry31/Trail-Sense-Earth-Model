53ecc77bffce4b0c3d40e05d9ce68f227547c33a
Kyle Corry <kylecorry31@gmail.com>
2023-11-18 09:21:31 -0500

    Don't convert humidity

--- generate-humidity-model.py
+++ generate-humidity-model.py
@@ -9,4 +9,4 @@ era5.download(start_year, end_year)
 era5.process_humidity(start_year, end_year)
 
 files = [f'images/{start_year}-{end_year}-{month}-humidity.tif' for month in range(1, 13)]
-compression.minify_multiple(files, lambda x: int(x * 2.55), -999, 'humidity', True, 100, False, (576, 361))
+compression.minify_multiple(files, lambda x: x, -999, 'humidity', True, 100, False, (576, 361))
