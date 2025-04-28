e308d97eaf5fb5556282f288005f1d30238f792b
Kyle Corry <kylecorry31@gmail.com>
2025-04-28 20:39:11 +0000

    Remove oceans from precipitation model

--- generate-precipitation-model.py
+++ generate-precipitation-model.py
@@ -1,4 +1,5 @@
-from scripts import era5, compression
+from scripts import era5, compression, natural_earth, load, to_tif, progress
+import numpy as np
 
 # Follow these instructions to sign in: https://cds.climate.copernicus.eu/how-to-api#install-the-cds-api-client
 
@@ -6,7 +7,30 @@ start_year = 1991
 end_year = 2020
 
 era5.download(start_year, end_year)
+natural_earth.download()
 era5.process_precipitation(start_year, end_year)
 
-files = [f'images/{start_year}-{end_year}-{month}-total_precipitation.tif' for month in range(1, 13)]
-compression.minify_multiple(files, lambda x: x, -999, 'precipitation', True, 100, False, (576, 361))
+files = [
+    f'images/{start_year}-{end_year}-{month}-total_precipitation.tif' for month in range(1, 13)]
+
+# Zero out the oceans of each file
+with progress.progress('Removing oceans', len(files)) as pbar:
+    for file in files:
+        image = np.array(load(file))
+        image = natural_earth.remove_oceans(image, 0)
+        to_tif(image, file.replace(
+            'total_precipitation', 'total_precipitation_land'))
+        pbar.update(1)
+
+files = [
+    f'images/{start_year}-{end_year}-{month}-total_precipitation_land.tif' for month in range(1, 13)]
+compression.minify_multiple(
+    files,
+    lambda x: x * 1000,
+    -999,
+    'precipitation',
+    True,
+    100,
+    False,
+    (720, 360)
+)
--- scripts/natural_earth.py
+++ scripts/natural_earth.py
@@ -2,6 +2,12 @@ import requests
 import os
 from .progress import progress
 import zipfile
+import geopandas as gpd
+import rasterio.transform
+import rasterio
+from scipy.ndimage import binary_dilation
+from rasterio.features import rasterize
+import numpy as np
 
 def __download(url, redownload=False):
     filename = url.split('/')[-1]
@@ -32,3 +38,40 @@ def download(redownload=False):
             with zipfile.ZipFile(f'source/natural-earth/{filename}', 'r') as zip_ref:
                 zip_ref.extractall(f'source/natural-earth')
             pbar.update(1)
+
+def remove_oceans(image, replacement=0, inverted=False, x_scale=0.25, y_scale=0.25, dilation=5):
+    shapefile_path = "source/natural-earth/ne_10m_land.shp"
+    island_shapefile_path = "source/natural-earth/ne_10m_minor_islands.shp"
+
+    gdf = gpd.read_file(shapefile_path)
+    gdf_islands = gpd.read_file(island_shapefile_path)
+
+    # Render the shapefiles to an image
+    width = image.shape[1]
+    height = image.shape[0]
+    scale = 4
+    mask = rasterize(gdf.geometry, out_shape=(height * scale, width * scale), transform=rasterio.transform.from_origin(-180, 90, x_scale / scale, y_scale / scale), dtype=np.float32)
+    mask[mask > 0] = 255
+
+    img_islands = rasterize(gdf_islands.geometry, out_shape=(height * scale, width * scale), transform=rasterio.transform.from_origin(-180, 90, x_scale / scale, y_scale / scale), dtype=np.float32)
+    img_islands[img_islands > 0] = 255
+
+    mask = np.maximum(mask, img_islands)
+
+    # Invert the mask
+    if inverted:
+        mask = 255 - mask
+
+    # Dilate the image
+    mask = mask > 0
+    mask = binary_dilation(mask, iterations=dilation * scale)
+    mask = mask.astype(np.float32)
+    
+    # Downsample the image
+    mask = mask[::scale, ::scale]
+
+    
+    image = image * mask
+    image[mask == 0] = replacement
+    return image
+    
