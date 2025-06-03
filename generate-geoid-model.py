381901e4948954585d063ddd429b466b36380f99
Kyle Corry <kylecorry31@gmail.com>
2025-06-03 10:14:10 +0000

    Fix geoid model generation

--- generate-geoid-model.py
+++ generate-geoid-model.py
@@ -1,4 +1,4 @@
 from scripts import etopo, compression
 
 etopo.download()
-compression.minify(etopo.geoid_path, lambda x: int(x), -99999, 'output/geoids.webp', 100, False, (361, 181))
+compression.minify(etopo.geoid_path, lambda x: x, -99999, 'output/geoids.webp', 100, False, (361, 181))
