35e66e58989269701e9e06cf58a970809c9a60c4
Kyle Corry <kylecorry31@gmail.com>
2025-06-28 20:17:22 +0000

    Add land mask image

---
+++ generate-land-mask.py
@@ -0,0 +1,22 @@
+from scripts import compression, natural_earth, to_tif
+from PIL import Image
+import numpy as np
+import os
+
+pixels_per_degree = 8
+
+# Increase the maximum image pixel limit to avoid DecompressionBombError
+Image.MAX_IMAGE_PIXELS = None
+
+natural_earth.download()
+
+if not os.path.exists('output'):
+    os.makedirs('output')
+
+# Create a white image of 360x180
+image = np.full((180 * pixels_per_degree, 360 * pixels_per_degree), 255, dtype=np.uint8)
+
+image = natural_earth.remove_oceans(image, scale=2, dilation=0)
+to_tif(image, 'images/land.tif')
+
+compression.minify('images/land.tif', lambda x: x, -99999, 'output/land.webp', 100, True, a_override=1, b_override=0)
