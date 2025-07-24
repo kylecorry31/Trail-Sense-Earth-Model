4c132c8ffee6a6a8d9b9b4c51e19e14a7088b7fa
Kyle Corry <kylecorry31@gmail.com>
2025-07-24 23:57:10 +0000

    Improve satellite map images

--- generate-satellite-maps.py
+++ generate-satellite-maps.py
@@ -1,10 +1,22 @@
-from scripts import visible_earth
+from scripts import visible_earth, natural_earth
 import PIL.Image as Image
+import numpy as np
 
 visible_earth.download()
 visible_earth.process_maps(0)
 
-for i in range(1, 13):
-    image = Image.open(f"images/world-map-{i}.tif")
-    image.thumbnail((3600, 3600))
-    image.save(f"output/world-map-{i}.webp", quality=75, lossless=False, format='WEBP')
+month = 7
+image = Image.open(f"images/world-map-{month}.tif")
+
+# Remove water
+image = np.array(image)
+image = natural_earth.remove_oceans(image, scale=2, dilation=0)
+image = natural_earth.remove_inland_water(image, scale=2, dilation=0)
+image = Image.fromarray(image)
+
+# Simplify colors
+image.thumbnail((3600, 3600))
+image = visible_earth.__simplify(image, image, 8)
+image.thumbnail((2400, 2400), resample=Image.NEAREST)
+
+image.save(f"output/world-map-{month}.webp", quality=100, lossless=True, format='WEBP', method=6, alpha_quality=0, optimize=True, compress_level=9)
