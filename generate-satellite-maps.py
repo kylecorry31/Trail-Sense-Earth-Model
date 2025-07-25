9609685d0dc00ee56d18f3499027a606da3f8be0
Kyle Corry <kylecorry31@gmail.com>
2025-07-25 11:10:18 +0000

    Improve satellite image

--- generate-satellite-maps.py
+++ generate-satellite-maps.py
@@ -13,8 +13,8 @@ reference = natural_earth.remove_inland_water(reference, scale=2, dilation=0)
 reference = Image.fromarray(reference)
 
 images = []
-with progress.progress('Processing world map', 6) as pbar:
-    for month in range(4, 10):
+with progress.progress('Processing world map', 4) as pbar:
+    for month in range(6, 10):
         image = Image.open(f"images/world-map-{month}.tif")
 
         # Remove water
@@ -23,21 +23,20 @@ with progress.progress('Processing world map', 6) as pbar:
         image = natural_earth.remove_inland_water(image, scale=2, dilation=0)
         image = Image.fromarray(image)
 
-        # Simplify colors
-        # TODO: Color based on climate zone instead or paint by hand
-        image = visible_earth.__simplify(reference, image, 8, 4, 4, format="RGB")
-        image.thumbnail((3600, 3600), resample=Image.NEAREST)
-
-        # Reapply water removal
-        image = np.array(image)
-        image = natural_earth.remove_oceans(image, scale=2, dilation=0)
-        image = natural_earth.remove_inland_water(image, scale=2, dilation=0)
-        image = Image.fromarray(image)
-
         images.append(image)
         pbar.update(1)
 
-image = np.median(np.array(images), axis=0).astype(np.uint8)
+image = np.mean(np.array(images), axis=0).astype(np.uint8)
+image = Image.fromarray(image)
+
+# Simplify colors
+image = visible_earth.__simplify(reference, image, 8, 4, 4, format="RGB")
+image.thumbnail((4200, 4200), resample=Image.NEAREST)
+
+# Reapply water removal
+image = np.array(image)
+image = natural_earth.remove_oceans(image, scale=2, dilation=0)
+image = natural_earth.remove_inland_water(image, scale=2, dilation=0)
 image = Image.fromarray(image)
 
 image.save("output/world-map.webp", quality=100, lossless=True, format='WEBP', method=6, alpha_quality=0, optimize=True, compress_level=9)
