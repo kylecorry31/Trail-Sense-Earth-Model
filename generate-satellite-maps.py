a0bee2390c2bec97f8c29dce09a5703b222187dc
Kyle Corry <kylecorry31@gmail.com>
2025-07-27 16:19:34 +0000

    Add grass to satellite map

--- generate-satellite-maps.py
+++ generate-satellite-maps.py
@@ -5,12 +5,13 @@ import numpy as np
 visible_earth.download()
 visible_earth.process_maps()
 
-colors = [
-    (0, 0, 0),  # Water
-    (200, 200, 200),  # Ice
-    (50, 70, 30),     # Forests
-    (160, 120, 80),  # Desert
-]
+DESERT = (180, 135, 90)
+FOREST = (50, 70, 30)
+RAINFOREST = (30, 50, 20)
+RED_SAND = (130, 80, 40)
+GRASS = (115, 110, 70)
+ICE = (200, 200, 200)
+WATER = (0, 0, 0)
 
 images = []
 with progress.progress('Loading world map', 12) as pbar:
@@ -19,38 +20,39 @@ with progress.progress('Loading world map', 12) as pbar:
         images.append(image)
         pbar.update(1)
 
-with progress.progress('Processing world map', 7) as pbar:
+with progress.progress('Processing world map', 8) as pbar:
     image = np.min(np.array(images), axis=0).astype(np.uint8)
     image = natural_earth.remove_oceans(image, scale=2, dilation=0)
     image = natural_earth.remove_inland_water(image, scale=2, dilation=0)
 
     # Desert
-    new_image = np.full_like(image, (180, 135, 90))
+    new_image = np.full_like(image, DESERT)
+    pbar.update(1)
+
+    # Grass
+    mask = (np.mean(image, axis=-1) < 100) & (image[..., 1] > image[..., 0] * 0.9)
+    new_image[mask] = GRASS
     pbar.update(1)
 
     # Forests (green is a large component, but overall not that bright)
-    mask = (np.mean(image, axis=-1) < 100) & (image[..., 1] > image[..., 0] * 0.9)
-    new_image[mask] = (50, 70, 30)
+    mask = (np.mean(image, axis=-1) < 100) & (image[..., 1] > image[..., 0] * 1.05)
+    new_image[mask] = FOREST
     pbar.update(1)
 
     # Rainforests
     mask = (mask) & (image[..., 1] < 40) & (image[..., 2] < 10) & (image[..., 0] < 30)
-    new_image[mask] = (30, 50, 20)
+    new_image[mask] = RAINFOREST
     pbar.update(1)
-
-    # # Arid
-    # mask = (image[..., 0] > image[..., 1]) & (image[..., 0] > image[..., 2] * 1.2) & (np.mean(image, axis=-1) < 90)
-    # new_image[mask] = (120, 100, 70)
 
     # Red sand
     mask = (image[..., 0] > image[..., 1] * 1.4) & (image[..., 0] > image[..., 2] * 1.4)
-    new_image[mask] = (130, 80, 40)
+    new_image[mask] = RED_SAND
     pbar.update(1)
 
     # Ice (r, g, b above threshold)
     threshold = 150
     mask = np.all(image >= threshold, axis=-1)
-    new_image[mask] = (200, 200, 200)
+    new_image[mask] = ICE
     pbar.update(1)
 
     # Fill all pixels below -60 lat or above 72 lat with white
@@ -60,8 +62,8 @@ with progress.progress('Processing world map', 7) as pbar:
     north_pixel = max(0, min(height, int((90 - north) / 180
         * height)))
     south_pixel = max(0, min(height, int((90 - south) / 180 * height)))
-    new_image[:north_pixel, :] = (200, 200, 200)
-    new_image[south_pixel:, :] = (200, 200, 200)
+    new_image[:north_pixel, :] = ICE
+    new_image[south_pixel:, :] = ICE
     pbar.update(1)
 
     image = new_image
@@ -71,11 +73,12 @@ with progress.progress('Processing world map', 7) as pbar:
     pbar.update(1)
 
 smoothing_order = [
-    (200, 200, 200),  # Ice
-    (30, 50, 20),     # Rainforests
-    (50, 70, 30),     # Forests
-    (130, 80, 40),    # Red sand
-    (180, 135, 90),   # Desert
+    ICE,
+    RAINFOREST,
+    FOREST,
+    GRASS,
+    RED_SAND,
+    DESERT,
 ]
 
 image_area = np.prod(mask.shape)
