46619e8ef12a3d2280842134c6cb209e0500cb9d
Kyle Corry <kylecorry31@gmail.com>
2025-08-19 23:32:57 +0000

    Improve star model

--- generate-star-model.py
+++ generate-star-model.py
@@ -1,142 +1,224 @@
 from scripts import simbad
 import numpy as np
 
-stars = [
-    'Sirius',
-    'Canopus',
-    'Rigil Kentaurus',
-    'Arcturus',
-    'Vega',
-    'Rigel',
-    'Procyon',
-    'Achernar',
-    'Betelgeuse',
-    'Hadar',
-    'Altair',
-    'Acrux',
-    'Aldebaran',
-    'Antares',
-    'Spica',
-    'Pollux',
-    'Fomalhaut',
-    'Deneb',
-    'Mimosa',
-    'Regulus',
-    'Adhara',
-    'Castor',
-    'Shaula',
-    'Gacrux',
-    'Bellatrix',
-    'Elnath',
-    'Miaplacidus',
-    'Alnilam',
-    'Alnair',
-    'Alnitak',
-    'Alioth',
-    'Dubhe',
-    'Mirfak',
-    'Wezen',
-    'Regor',
-    'Sargas',
-    'Kaus Australis',
-    'Avior',
-    'Alkaid',
-    'Menkalinan',
-    'Atria',
-    'Alhena',
-    'Peacock',
-    'Alsephina',
-    'Mirzam',
-    'Polaris',
-    'Alphard',
-    'Hamal',
-    'Diphda',
-    'Alpheratz',
-    'Ankaa',
-    'Schedar',
-    'Acamar',
-    'Menkar',
-    'Capella',
-    'Suhail',
-    'Denebola',
-    'Gienah',
-    'Menkent',
-    'Zubenelgenubi',
-    'Kochab',
-    'Alphecca',
-    'Sabik',
-    'Rasalhague',
-    'Eltanin',
-    'Nunki',
-    'Enif',
-    'Markab',
-    'Merak',
-    'Phecda',
-    'Megrez',
-    'Mizar',
-    'Imai',
-    'Ginan',
-    'Saiph',
-    'Meissa',
-    'Mintaka',
-    'Mirach',
-    'Algieba',
-    'Algol',
-    'Tiaki',
-    'Muhlifain',
-    'Aspidiske',
-    'Sadr',
-    'Naos',
-    'Almach',
-    'Caph',
-    'Uridim',
-    'Epsilon Centauri',
-    'Dschubba',
-    'Larawag',
-    'Eta Centauri',
-    'Girtab',
-    'Scheat',
-    'Aludra',
-    'Alderamin',
-    # 'Markeb',
-    'Gamma Cassiopeiae',
-    'Aljanah',
-    'Acrab',
-    'Ruchbah',
-    'Segin',
-    'Izar'
+constellation_abbreviation_to_name = {
+    "And": "Andromedae",
+    "Ant": "Antliae",
+    "Aps": "Apodis",
+    "Aqr": "Aquarii",
+    "Aql": "Aquilae",
+    "Ara": "Arae",
+    "Ari": "Arietis",
+    "Aur": "Aurigae",
+    "Boo": "Boötis",
+    "Cae": "Caeli",
+    "Cam": "Camelopardalis",
+    "Cnc": "Cancri",
+    "CVn": "Canum Venaticorum",
+    "CMa": "Canis Majoris",
+    "CMi": "Canis Minoris",
+    "Cap": "Capricorni",
+    "Car": "Carinae",
+    "Cas": "Cassiopeiae",
+    "Cen": "Centauri",
+    "Cep": "Cephei",
+    "Cet": "Ceti",
+    "Cha": "Chamaeleontis",
+    "Cir": "Circini",
+    "Col": "Columbae",
+    "Com": "Comae Berenices",
+    "CrA": "Coronae Australis",
+    "CrB": "Coronae Borealis",
+    "Crv": "Corvi",
+    "Crt": "Crateris",
+    "Cru": "Crux",
+    "Cyg": "Cygni",
+    "Del": "Delphini",
+    "Dor": "Doradus",
+    "Dra": "Draconis",
+    "Equ": "Equulei",
+    "Eri": "Eridani",
+    "For": "Fornacis",
+    "Gem": "Geminorum",
+    "Gru": "Gruis",
+    "Her": "Herculis",
+    "Hor": "Horologii",
+    "Hya": "Hydrae",
+    "Hyi": "Hydri",
+    "Ind": "Indi",
+    "Lac": "Lacertae",
+    "Leo": "Leonis",
+    "LMi": "Leonis Minoris",
+    "Lep": "Leporis",
+    "Lib": "Librae",
+    "Lup": "Lupi",
+    "Lyn": "Lyncis",
+    "Lyr": "Lyrae",
+    "Men": "Mensae",
+    "Mic": "Microscopii",
+    "Mon": "Monocerotis",
+    "Mus": "Muscae",
+    "Nor": "Normae",
+    "Oct": "Octantis",
+    "Oph": "Ophiuchi",
+    "Ori": "Orionis",
+    "Pav": "Pavonis",
+    "Peg": "Pegasi",
+    "Per": "Persei",
+    "Phe": "Phoenicis",
+    "Pic": "Pictoris",
+    "Pis": "Piscium",
+    "PsA": "Piscis Austrini",
+    "Pup": "Puppis",
+    "Pyx": "Pyxidis",
+    "Ret": "Reticuli",
+    "Sge": "Sagittae",
+    "Sgr": "Sagittarii",
+    "Sco": "Scorpii",
+    "Scl": "Sculptoris",
+    "Sct": "Scuti",
+    "Ser": "Serpentis",
+    "Sex": "Sextantis",
+    "Tau": "Tauri",
+    "Tel": "Telescopii",
+    "Tri": "Trianguli",
+    "TrA": "Trianguli Australis",
+    "Tuc": "Tucanae",
+    "UMa": "Ursae Majoris",
+    "UMi": "Ursae Minoris",
+    "Vel": "Velorum",
+    "Vir": "Virginis",
+    "Vol": "Volantis",
+    "Vul": "Vulpeculae"
+}
+
+greek_letters = {
+    "alf": "Alpha",
+    "alf01": "Alpha 1",
+    "alf02": "Alpha 2",
+    "bet": "Beta",
+    "bet01": "Beta 1",
+    "bet02": "Beta 2",
+    "gam": "Gamma",
+    "gam01": "Gamma 1",
+    "gam02": "Gamma 2",
+    "del": "Delta",
+    "del01": "Delta 1",
+    "del02": "Delta 2",
+    "eps": "Epsilon",
+    "zet": "Zeta",
+    "zet01": "Zeta 1",
+    "zet02": "Zeta 2",
+    "eta": "Eta",
+    "eta01": "Eta 1",
+    "eta02": "Eta 2",
+    "tet": "Theta",
+    "iot": "Iota",
+    "iot01": "Iota 1",
+    "iot02": "Iota 2",
+    "kap": "Kappa",
+    "lam": "Lambda",
+    "mu.": "Mu",
+    "mu.01": "Mu 1",
+    "mu.02": "Mu 2",
+    "nu": "Nu",
+    "xi": "Xi",
+    "omi": "Omicron",
+    "pi.": "Pi",
+    "rho": "Rho",
+    "sig": "Sigma",
+    "tau": "Tau",
+    "ups": "Upsilon",
+    "phi": "Phi",
+    "chi": "Chi",
+    "psi": "Psi",
+    "ome": "Omega"
+}
+
+proper_name_map = {
+    'Alpha Andromedae': 'Alpheratz',
+    'Beta Centauri': 'Hadar',
+    'Alpha Canis Majoris': 'Sirius',
+    'Alpha Canis Minoris': 'Procyon',
+    'Alpha 1 Crux': 'Acrux',
+    'Alpha Geminorum': 'Castor',
+    'Alpha Lupi': 'Uridim',
+    'Beta Persei': 'Algol',
+    'Tau Puppis': 'Tau Puppis',
+    'Iota 1 Scorpii': 'Apollyon',
+    'Alpha Tucanae': 'Lang-Exster',
+    'Eta Ursae Majoris': 'Alkaid',
+    'Zeta 1 Ursae Majoris': 'Mizar',
+    'Alpha Ursae Minoris': 'Polaris',
+    'Kappa Velorum': 'Markeb',
+    'Alpha Centauri': 'Rigil Kentaurus',
+    'Epsilon Cassiopeiae': 'Segin',
+    'Lambda Orionis': 'Meissa',
+    'Delta Ursae Majoris': 'Megrez',
+    'Epsilon Crux': 'Ginan',
+    'Theta Eridani': 'Acamar',
+    'Delta 1 Velorum': 'Alsephina'
+}
+
+ignored_stars = [
+    'Alpha 2 Crux',
+    'Gamma 1 Leonis',
+    'Beta 1 Scorpii'
 ]
 
+stars = simbad.get_bright_objects(3.0)
+additional_stars = [
+    '* eps Cas', # Segin
+    '* lam Ori', # Meissa
+    '* del UMa', # Megrez
+    '* eps Cru', # Ginan
+    '* tet Eri', # Acamar
+    '* del01 Vel' # Alsephina
+]
+
+for star_name in additional_stars:
+    star_details = simbad.get_star_details(star_name)
+    if star_details is not None:
+        stars.append(star_details)
+
+stars = [star for star in stars if star[0].startswith('*')]
+
+to_remove = []
 for star in stars:
-    id, ra, dec, v_mag, pm_ra, pm_dec, color_index = simbad.get_star_details(star)
-    print(f"Star(\"{star}\", EquatorialCoordinate({dec}, {ra}), {v_mag}f, ProperMotion({pm_dec}, {pm_ra}), {(str(color_index) + 'f') if not np.isnan(color_index) else 'null'}),")  
+    id = star[0]
+    last = id.split(' ')[-1]
+    if len(last) == 1 and last.isalpha():
+        # See if there's a star with the same base id
+        base_id = id[:-1].strip()
+        if any(s[0] == base_id for s in stars):
+            to_remove.append(star)
 
-# stars = simbad.get_bright_objects()
-# stars = [star for star in stars if star[0].startswith('*')]
+for star in to_remove:
+    stars.remove(star)
 
-# to_remove = []
-# for star in stars:
-#     id = star[0]
-#     last = id.split(' ')[-1]
-#     if len(last) == 1 and last.isalpha():
-#         # See if there's a star with the same base id
-#         base_id = id[:-1].strip()
-#         if any(s[0] == base_id for s in stars):
-#             to_remove.append(star)
+# Sort stars by the second word in the ID and then by the first word
+stars.sort(key=lambda x: x[0][1:].strip().split(' ')[1].lower() + " " + x[0][1:].strip().split(' ')[0].lower())
 
-# for star in to_remove:
-#     stars.remove(star)
+found = []
 
-# # Sort stars by the second word in the ID and then by the first word
-# stars.sort(key=lambda x: x[0][1:].strip().split(' ')[1].lower() + " " + x[0][1:].strip().split(' ')[0].lower())
+for star in stars:
+    id = star[0][1:].strip()
+    split_id = [w for w in id.split(' ') if w.strip()]
+    latin_name = greek_letters[split_id[0]] + " " + constellation_abbreviation_to_name[split_id[1]]
+    if latin_name in ignored_stars:
+        continue
+    name = f'"{",".join([name.title() for name in star[1]])}"'
+    if len(star[1]) == 0:
+        name = f'"{latin_name}"'
+    if latin_name in proper_name_map:
+        name = f'"{proper_name_map[latin_name]}"'
+    
+    found.append(name.replace('"', ''))
 
-# for star in stars:
-#     id = star[0][1:].strip()
-#     names = f'"{",".join([name.title() for name in star[1]])}"'
-#     ra = star[2]
-#     dec = star[3]
-#     v_mag = star[4]
-#     pm_ra = star[5]
-#     pm_dec = star[6]
-#     color_index = star[7]
-#     print(f"Star(\"{id}\", {names}, EquatorialCoordinate({dec}, {ra}), {v_mag}f, ProperMotion({pm_dec}, {pm_ra}), {(str(color_index) + 'f') if not np.isnan(color_index) else 'null'}),")
+    ra = star[2]
+    dec = star[3]
+    v_mag = star[4]
+    pm_ra = star[5]
+    pm_dec = star[6]
+    color_index = star[7]
+    print(f"Star(\"{latin_name}\", {name}, EquatorialCoordinate({dec}, {ra}), {v_mag}f, ProperMotion({pm_dec}, {pm_ra}), {(str(color_index) + 'f') if not np.isnan(color_index) else 'null'}),")
--- scripts/simbad.py
+++ scripts/simbad.py
@@ -3,49 +3,25 @@ from astropy.coordinates import Angle
 import numpy as np
 
 magnitude_map = {
-    'Acrux': 0.76,
-    'Alsephina': 1.95,
-    'Acamar': 3.2,
-    'Mizar': 2.04,
-    'Markeb': 2.48
+    '* del01 Vel': 1.95,
+    '* tet Eri': 3.2
 }
 
 color_index_map = {
     '* bet Cen': -0.23,
-    'Hadar': -0.23,
-    'Acrux': -0.26,
-    'Alsephina': 0.04,
-    'Acamar': 0.128,
-    'Mizar': 0.02,
-    'Markeb': -0.2
+    '* del01 Vel': 0.04,
+    '* tet Eri': 0.128
 }
 
 proper_motion_ra_map = {
-    'Acrab': -5.2,
     '* bet Sco': -5.2
 }
 
 proper_motion_dec_map = {
-    'Acrab': -24.04,
     '* bet Sco': -24.04
 }
 
-main_id_map = {
-    'Mizar': '* zet01 UMa',
-    'Markeb': 'CD-26 4707',
-    'Girtab': '* kap Sco',
-    'Muhlifain': '* gam Cen',
-    'Uridim': '* alf Lup'
-}
-
-def get_star_details(star_name):
-    # Look up the object ID for the star name
-    if star_name in main_id_map:
-        main_id = main_id_map[star_name]
-    else:
-        result_table = Simbad.query_objectids(star_name)
-        main_id = result_table["id"].data[0]
-
+def get_star_details(main_id):
     # Query SIMBAD for the main ID
     Simbad.add_votable_fields("flux(V)", "pmra", "pmdec", "flux(B)")
     result_table = Simbad.query_object(main_id)
@@ -53,16 +29,16 @@ def get_star_details(star_name):
     dec = Angle(result_table["dec"].data[0], unit='deg').degree
     v_mag = result_table["V"].data[0]
     if np.ma.is_masked(v_mag):
-        v_mag = magnitude_map.get(star_name, np.nan)
+        v_mag = magnitude_map.get(main_id, np.nan)
 
     pm_ra = result_table["pmra"].data[0]
     if np.ma.is_masked(pm_ra):
-        pm_ra = proper_motion_ra_map.get(star_name, np.nan)
+        pm_ra = proper_motion_ra_map.get(main_id, np.nan)
     pm_ra = pm_ra / (1000 * 3600)
 
     pm_dec = result_table["pmdec"].data[0]
     if np.ma.is_masked(pm_dec):
-        pm_dec = proper_motion_dec_map.get(star_name, np.nan)
+        pm_dec = proper_motion_dec_map.get(main_id, np.nan)
     pm_dec = pm_dec / (1000 * 3600)
 
     b_mag = result_table["B"].data[0]
@@ -72,9 +48,10 @@ def get_star_details(star_name):
     color_index = b_mag - v_mag
 
     if np.isnan(color_index):
-        color_index = color_index_map.get(star_name, np.nan)
+        color_index = color_index_map.get(main_id, np.nan)
 
-    return main_id, float(ra), float(dec), float(v_mag), float(pm_ra), float(pm_dec), float(color_index)
+    return main_id, [main_id], float(ra), float(dec), float(v_mag), float(pm_ra), float(pm_dec), float(color_index)
+
 
 def get_bright_objects(max_v_mag: float = 4.0):
     """
@@ -96,6 +73,7 @@ def get_bright_objects(max_v_mag: float = 4.0):
 
     # Build a case-insensitive column lookup
     colmap = {c.lower(): c for c in result_table.colnames}
+
     def col(name: str) -> str:
         return colmap.get(name.lower(), name)
 
@@ -103,9 +81,11 @@ def get_bright_objects(max_v_mag: float = 4.0):
         try:
             if ids_value is None or (hasattr(ids_value, 'mask') and getattr(ids_value, 'mask', False)):
                 raise ValueError
-            s = ids_value.decode('utf-8', errors='ignore') if isinstance(ids_value, (bytes, bytearray)) else str(ids_value)
+            s = ids_value.decode('utf-8', errors='ignore') if isinstance(
+                ids_value, (bytes, bytearray)) else str(ids_value)
             parts = [p.strip() for p in s.split('|')]
-            names = [p[4:].strip() for p in parts if p.upper().startswith('NAME ')]
+            names = [p[4:].strip()
+                     for p in parts if p.upper().startswith('NAME ')]
             return names
         except Exception:
             pass
@@ -119,13 +99,13 @@ def get_bright_objects(max_v_mag: float = 4.0):
         main_id = str(main_id)
 
         # Common/user-readable name from IDS
-        ids_value = row[col('IDS')] if col('IDS') in result_table.colnames else None
+        ids_value = row[col('IDS')] if col(
+            'IDS') in result_table.colnames else None
         names = extract_common_name(ids_value)
-
         # Coordinates
         ra_str = row[col('RA')]
         dec_str = row[col('DEC')]
-        ra = Angle(ra_str, unit='hourangle').degree
+        ra = Angle(ra_str, unit='deg').degree
         dec = Angle(dec_str, unit='deg').degree
 
         # Photometry
@@ -138,15 +118,18 @@ def get_bright_objects(max_v_mag: float = 4.0):
             b_mag = np.nan
 
         # Color index (B - V)
-        color_index = b_mag - v_mag if not (np.isnan(b_mag) or np.isnan(v_mag)) else np.nan
+        color_index = b_mag - \
+            v_mag if not (np.isnan(b_mag) or np.isnan(v_mag)) else np.nan
         if np.isnan(color_index):
             color_index = color_index_map.get(main_id, np.nan)
 
         # Proper motion (convert mas/yr to deg/yr)
-        pm_ra = row[col('PMRA')] if col('PMRA') in result_table.colnames else np.nan
+        pm_ra = row[col('PMRA')] if col(
+            'PMRA') in result_table.colnames else np.nan
         if np.ma.is_masked(pm_ra):
             pm_ra = proper_motion_ra_map.get(main_id, np.nan)
-        pm_dec = row[col('PMDEC')] if col('PMDEC') in result_table.colnames else np.nan
+        pm_dec = row[col('PMDEC')] if col(
+            'PMDEC') in result_table.colnames else np.nan
         if np.ma.is_masked(pm_dec):
             pm_dec = proper_motion_dec_map.get(main_id, np.nan)
 
