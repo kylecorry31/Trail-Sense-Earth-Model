1f9f10f57e122831f8ada14d89829f97c0b33ffc
Kyle Corry <kylecorry31@gmail.com>
2026-01-10 13:28:01 -0500

    Use operations for land mask

--- generate-land-mask.py
+++ generate-land-mask.py
@@ -1,4 +1,7 @@
-from scripts import compression, natural_earth, to_tif
+from scripts import natural_earth, to_tif, load_pixels
+from scripts.operators import process
+from scripts.operators.compression import LinearCompression
+from scripts.operators.basic import Save
 from PIL import Image
 import numpy as np
 import os
@@ -10,14 +13,12 @@ Image.MAX_IMAGE_PIXELS = None
 
 natural_earth.download()
 
-if not os.path.exists('output'):
-    os.makedirs('output')
+if not os.path.exists("output"):
+    os.makedirs("output")
 
 # Create a white image of 360x180
 image = np.full((2100, 4200), 255, dtype=np.uint8)
 
 image = natural_earth.remove_oceans(image, scale=2, dilation=0)
 image = natural_earth.remove_inland_water(image, scale=2, dilation=0)
-to_tif(image, 'images/land.tif')
-
-compression.minify('images/land.tif', lambda x: x, -99999, 'output/land.webp', 100, True, a_override=1, b_override=0)
+process([image], Save(["output/land.webp"], quality=100, lossless=True))
--- scripts/operators/__init__.py
+++ scripts/operators/__init__.py
@@ -1,4 +1,9 @@
+from .. import load_pixels
+
 def process(images, *operators):
+    if images and isinstance(images[0], str):
+        images = [load_pixels(path) for path in images]
+    
     results = []
     for operator in operators:
         images, data = operator.apply(images)
