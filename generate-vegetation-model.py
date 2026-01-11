5245a2ea8220fd51d50ce72a44d208113ac9c927
Kyle Corry <kylecorry31@gmail.com>
2026-01-11 12:29:55 -0500

    refactor: update vegetation model to use operations

--- generate-vegetation-model.py
+++ generate-vegetation-model.py
@@ -1,5 +1,5 @@
-from scripts import era5, compression
-import os
+from scripts import era5
+from scripts.operators import process, Resize, Group, Save, Replace
 
 # Follow these instructions to sign in: https://cds.climate.copernicus.eu/how-to-api#install-the-cds-api-client
 
@@ -11,7 +11,11 @@ era5.process_vegetation(end_year)
 
 files = [f'images/{end_year}-{end_year}-7-high_vegetation.tif',
          f'images/{end_year}-{end_year}-7-low_vegetation.tif']
-compression.minify_multiple(files, lambda x: x, -999, 'vegetation',
-                            True, 100, True, (576, 361), a_override=1, b_override=0)
 
-os.rename('output/vegetation-1-3.webp', 'output/vegetation.webp')
+process(
+    files,
+    Replace(-999, 0),
+    Resize((576, 361), exact=True),
+    Group(2),
+    Save(['output/vegetation.webp'], quality=100, lossless=True)
+)
