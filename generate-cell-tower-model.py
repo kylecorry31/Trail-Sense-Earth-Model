98c3da6f086df68f19249ad15d3a04321d7222db
Kyle Corry <kylecorry31@gmail.com>
2026-01-10 20:27:12 -0500

    Use operators for cell tower model

--- generate-cell-tower-model.py
+++ generate-cell-tower-model.py
@@ -1,6 +1,6 @@
-from scripts import opencellid, compression, load_pixels, fcc, mls, cell_towers
+from scripts import opencellid, load_pixels, fcc, mls, cell_towers
 from scripts.operators import process
-from scripts.operators.basic import Tile, Save, BitwiseOr
+from scripts.operators.basic import Tile, Save, BitwiseOr, Type
 import os
 import shutil
 from PIL import Image
@@ -9,17 +9,6 @@ from scripts.operators.masking import RemoveOceans
 
 resolution = 0.01
 
-def get_tile_size(width, height, max_width, max_height):
-    dividers = [5, 4, 3, 2, 1]
-    new_width = width
-    new_height = height
-    for divider in dividers:
-        if width % divider == 0 and width // divider < max_width:
-            new_width = width // divider
-        if height % divider == 0 and height // divider < max_height:
-            new_height = height // divider
-    return new_width, new_height
-
 
 fcc.download()
 opencellid.download()
@@ -27,41 +16,25 @@ opencellid.download()
 mls.download()
 
 fcc.process_towers(resolution)
-towers = cell_towers.get_all_towers(opencellid.csv_path, 'OpenCellID')
-towers = cell_towers.get_all_towers(mls.csv_path, 'MLS', towers)
-cell_towers.create_tower_image(towers, resolution, 'images/cell_towers')
+towers = cell_towers.get_all_towers(opencellid.csv_path, "OpenCellID")
+towers = cell_towers.get_all_towers(mls.csv_path, "MLS", towers)
+cell_towers.create_tower_image(towers, resolution, "images/cell_towers")
 
-Image.MAX_IMAGE_PIXELS = None
-
-fcc_image = load_pixels('images/fcc/cell_towers.tif').astype(np.uint8)
-cell_towers_image = load_pixels('images/cell_towers/cell_towers.tif').astype(np.uint8)
-
-total_width = fcc_image.shape[1]
-total_height = fcc_image.shape[0]
 max_width = 12000
 max_height = 9000
 
-tile_width, tile_height = get_tile_size(total_width, total_height, max_width, max_height)
-print(f'Tiling into {tile_width}x{tile_height} tiles')
-print(f'Rows: {total_height // tile_height}, Columns: {total_width // tile_width}')
+shutil.rmtree("output/towers", ignore_errors=True)
 
-shutil.rmtree('images/cell_towers_processed/towers', ignore_errors=True)
-os.makedirs('images/cell_towers_processed/towers', exist_ok=True)
+tile_operator_index = 3
 
-shutil.rmtree('output/towers', ignore_errors=True)
-os.makedirs('output/towers', exist_ok=True)
-
-process(
-    [fcc_image, cell_towers_image],
+_, metadata = process(
+    ["images/fcc/cell_towers.tif", "images/cell_towers/cell_towers.tif"],
+    Type(np.uint8),
     BitwiseOr(),
     RemoveOceans(dilation=0, scale=1),
-    Tile((tile_height, tile_width)),
-    Save(lambda x: f'images/cell_towers_processed/towers/cell_towers_{x}.tif', mode='F')
+    Tile((max_height, max_width), max_dimensions=True),
+    Save(lambda x: f"output/towers/cell_towers_{x}.webp", quality=100, lossless=True),
+    show_progress=True
 )
 
-paths = [f'images/cell_towers_processed/towers/{path}' for path in os.listdir('images/cell_towers_processed/towers')]
-
-for path in paths:
-    compression.minify(path, lambda x: x, -99999, path.replace('images/cell_towers_processed/towers/', 'output/towers/').replace('.tif', '.webp'), 100, True, a_override=1, b_override=0)
-
-shutil.rmtree('images/cell_towers_processed', ignore_errors=True)
+print(metadata[tile_operator_index]['tiles'][0])
--- scripts/operators/basic.py
+++ scripts/operators/basic.py
@@ -191,23 +191,52 @@ class BitwiseOr(ImageOperator):
 
 
 class Tile(ImageOperator):
-    def __init__(self, tile_size):
+    def __init__(self, tile_size, max_dimensions=False):
         self.tile_size = tile_size
+        self.max_dimensions = max_dimensions
+
+    def __get_tile_size(self, image_shape):
+        max_height, max_width = self.tile_size
+        width = image_shape[1]
+        height = image_shape[0]
+        dividers = [5, 4, 3, 2, 1]
+        tile_width = width
+        tile_height = height
+        for divider in dividers:
+            if width % divider == 0 and width // divider < max_width:
+                tile_width = width // divider
+            if height % divider == 0 and height // divider < max_height:
+                tile_height = height // divider
+        return tile_height, tile_width
 
     def apply(self, images):
         output = []
+        tile_metadata = []
         for image in images:
-            tiles_x = int(np.ceil(image.shape[1] / self.tile_size[1]))
-            tiles_y = int(np.ceil(image.shape[0] / self.tile_size[0]))
+            tile_height, tile_width = (
+                self.__get_tile_size(image.shape)
+                if self.max_dimensions
+                else self.tile_size
+            )
+            tiles_x = int(np.ceil(image.shape[1] / tile_width))
+            tiles_y = int(np.ceil(image.shape[0] / tile_height))
+            tile_metadata.append(
+                {
+                    "tile_width": tile_width,
+                    "tile_height": tile_height,
+                    "rows": tiles_y,
+                    "columns": tiles_x,
+                }
+            )
             for ty in range(tiles_y):
                 for tx in range(tiles_x):
-                    x_start = tx * self.tile_size[1]
-                    y_start = ty * self.tile_size[0]
-                    x_end = min(x_start + self.tile_size[1], image.shape[1])
-                    y_end = min(y_start + self.tile_size[0], image.shape[0])
+                    x_start = tx * tile_width
+                    y_start = ty * tile_height
+                    x_end = min(x_start + tile_width, image.shape[1])
+                    y_end = min(y_start + tile_height, image.shape[0])
                     tile = image[y_start:y_end, x_start:x_end]
                     output.append(tile)
-        return output, {}
+        return output, {"tiles": tile_metadata}
 
 
 class Group(ImageOperator):
