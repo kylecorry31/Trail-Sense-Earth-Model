626d0aab2939527be9d7f960ef31dd559b20c085
Kyle Corry <kylecorry31@gmail.com>
2026-01-11 08:52:21 -0500

    Use operations for DEM images

--- generate-dem.py
+++ generate-dem.py
@@ -1,4 +1,7 @@
-from scripts import etopo, compression, natural_earth, progress, clip, to_tif, load_pixels, resize, reshape
+from scripts import etopo, natural_earth, progress, load_pixels
+from scripts.operators import process
+from scripts.operators.compression import Split16Bits, LinearCompression
+from scripts.operators.basic import Conditional, Group, Reshape, Save, Type
 from PIL import Image
 import numpy as np
 import os
@@ -6,7 +9,9 @@ import re
 import json
 import zipfile
 
-version = '0.3.0'
+from scripts.operators.masking import Mask, RemoveInlandWater, RemoveOceans
+
+version = "0.3.0"
 preset_filter = None
 
 presets = [
@@ -17,7 +22,7 @@ presets = [
         "compression_quality": 100,
         "above_N60_quality": 100,
         "above_N60_scale": 1.0,
-        "ignore_threshold": 2
+        "ignore_threshold": 2,
     },
     {
         "id": "medium",
@@ -26,7 +31,7 @@ presets = [
         "compression_quality": 100,
         "above_N60_quality": 100,
         "above_N60_scale": 1.0,
-        "ignore_threshold": 2
+        "ignore_threshold": 2,
     },
     {
         "id": "low",
@@ -35,7 +40,7 @@ presets = [
         "compression_quality": 90,
         "above_N60_quality": 60,
         "above_N60_scale": 0.75,
-        "ignore_threshold": 8
+        "ignore_threshold": 8,
     },
     {
         "id": "mini",
@@ -44,8 +49,8 @@ presets = [
         "compression_quality": 75,
         "above_N60_quality": 10,
         "above_N60_scale": 0.1,
-        "ignore_threshold": 20
-    }
+        "ignore_threshold": 20,
+    },
 ]
 
 # Increase the maximum image pixel limit to avoid DecompressionBombError
@@ -56,147 +61,192 @@ etopo.download(surface_resolution=resolution)
 natural_earth.download()
 
 for preset in presets:
-    if preset_filter is not None and preset['id'] != preset_filter:
+    if preset_filter is not None and preset["id"] != preset_filter:
         continue
 
-    scale = preset['scale']
-    compress_images = preset['compress_images']
-    compression_quality = preset['compression_quality']
-    ignore_threshold = preset['ignore_threshold']
-    above_N60_quality = preset['above_N60_quality']
-    above_N60_scale = preset['above_N60_scale']
+    scale = preset["scale"]
+    compress_images = preset["compress_images"]
+    compression_quality = preset["compression_quality"]
+    ignore_threshold = preset["ignore_threshold"]
+    above_N60_quality = preset["above_N60_quality"]
+    above_N60_scale = preset["above_N60_scale"]
 
     current_version = f'{version}-{preset["id"]}'
 
     true_resolution = int(resolution / scale)
 
-    files = etopo.get_file_paths(resolution, 'surface')
+    files = etopo.get_file_paths(resolution, "surface")
 
-    if os.path.exists('output/dem'):
-        for file in os.listdir('output/dem'):
-            os.remove(os.path.join('output/dem', file))
+    if os.path.exists("output/dem"):
+        for file in os.listdir("output/dem"):
+            os.remove(os.path.join("output/dem", file))
 
-    if not os.path.exists('output/dem'):
-        os.makedirs('output/dem')
+    if not os.path.exists("output/dem"):
+        os.makedirs("output/dem")
 
-    with progress.progress(f'Processing DEM files ({preset["id"]})', len(files)) as pbar:
+    with progress.progress(
+        f'Processing DEM files ({preset["id"]})', len(files)
+    ) as pbar:
         compression_factors = []
         for file in files:
-            region = file.split('_')[-2]
+            region = file.split("_")[-2]
 
-            if region.startswith('S60') or region.startswith('S75') or region.startswith('N90'):
+            if (
+                region.startswith("S60")
+                or region.startswith("S75")
+                or region.startswith("N90")
+            ):
                 pbar.update(1)
                 continue
 
             # N75 covers 60 - 75, 75-90 are already removed
-            actual_quality = above_N60_quality if region.startswith(
-                'N75') else compression_quality
-            actual_scale = above_N60_scale if region.startswith(
-                'N75') else scale
+            actual_quality = (
+                above_N60_quality if region.startswith("N75") else compression_quality
+            )
+            actual_scale = above_N60_scale if region.startswith("N75") else scale
 
             # First letter is either N or S followed by 2 digits and E or W and then 3 digits
-            regex = r'([NS]\d{2})([EW]\d{3})'
+            regex = r"([NS]\d{2})([EW]\d{3})"
             match = re.search(regex, region)
             if not match:
-                print(f'Invalid region format: {region}')
+                print(f"Invalid region format: {region}")
                 pbar.update(1)
                 continue
             latitude, longitude = match.groups()
 
-            latitude = int(latitude[1:]) * (1 if latitude[0] == 'N' else -1)
-            longitude = int(longitude[1:]) * (1 if longitude[0] == 'E' else -1)
+            latitude = int(latitude[1:]) * (1 if latitude[0] == "N" else -1)
+            longitude = int(longitude[1:]) * (1 if longitude[0] == "E" else -1)
 
             end_latitude = latitude - resolution
             end_longitude = longitude + resolution
 
             initial_size = 3600
-            image_size = (int(initial_size * actual_scale), int(initial_size *
-                          actual_scale)) if actual_scale is not None else None
+            image_size = (
+                (int(initial_size * actual_scale), int(initial_size * actual_scale))
+                if actual_scale is not None
+                else None
+            )
 
             # While the source image removes most of the water, this will get ones missed in the US
             # Look into switching to https://www.earthdata.nasa.gov/data/catalog/lpcloud-mod44w-061
-            image = natural_earth.remove_oceans_from_tif(file, 'images/dem_no_oceans.tif', scale=2, bbox=(
-                longitude, end_latitude, end_longitude, latitude), dilation=-20, only_replace_negative_pixels=True)
-            image = natural_earth.remove_inland_water(image, scale=2, bbox=(
-                longitude, end_latitude, end_longitude, latitude), replacement=0, dilation=-1, only_replace_negative_pixels=True)
-            source_image = load_pixels(file.replace('surface', 'surface_sid'))
 
-            # Zero out image where the source is bathymetry
-            image[~np.isin(source_image, [9, 10, 11, 13])] = 0
+            source_image = load_pixels(file.replace("surface", "surface_sid"))
 
-            # If it is 13 and below 0, set it to 0 (13 is US coastline)
-            image[(source_image == 13) & (image < 0)] = 0
+            mask, _ = process(
+                [file],
+                RemoveOceans(
+                    scale=2,
+                    dilation=-20,
+                    only_replace_negative_pixels=True,
+                    bbox=(longitude, end_latitude, end_longitude, latitude),
+                ),
+                RemoveInlandWater(
+                    scale=2,
+                    dilation=-1,
+                    only_replace_negative_pixels=True,
+                    bbox=(longitude, end_latitude, end_longitude, latitude),
+                ),
+                # Remove bathymetry areas based on the sid image
+                Mask(~np.isin(source_image, [9, 10, 11, 13])),
+                # If it is 13 and below 0, set it to 0 (13 is US coastline)
+                Mask(lambda image: (source_image == 13) & (image < 0)),
+                # Image is not allowed to be lower than the lowest place on land
+                Mask(lambda image: image < -430.5),
+                # This image has inland water that is not properly masked
+                Conditional(region == "S30W060", Mask(lambda image: image < 0)),
+                Type(np.bool),
+            )
+            mask = mask[0]
 
-            # Image is not allowed to be lower than the lowest place on land
-            image[image < -430.5] = 0
-
-            # This image has inland water that is not properly masked
-            if region == 'S30W060':
-                image[image < 0] = 0
-
-            image = reshape(image, image_size)
+            # Reapply the mask
+            image, _ = process([file], Mask(mask, invert=True), Reshape(image_size))
+            image = image[0]
 
             # Skip images with no valid land data
             if np.all(image < ignore_threshold):
                 pbar.update(1)
                 continue
 
-            to_tif(image, 'images/dem_no_oceans.tif')
-
             if compress_images:
-                a, b = compression.minify('images/dem_no_oceans.tif', lambda x: x, -99999,
-                                          f'output/dem/{region}.webp', actual_quality, False, should_print=False)
-                if a == 1 and b == 0:
-                    # No actual data
-                    print(f'Skipping {region} - no data after resizing.')
-                    pbar.update(1)
-                    continue
-                compression_factors.append({
-                    "filename": f'{region}.webp',
-                    "a": float(a),
-                    "b": float(b),
-                    "latitude_start": float(latitude),
-                    "longitude_start": float(longitude),
-                    "latitude_end": float(end_latitude),
-                    "longitude_end": float(end_longitude),
-                    "width": int(image_size[0]),
-                    "height": int(image_size[1])
-                })
+                # 8-bit compression
+                _, results = process(
+                    [image],
+                    LinearCompression(),
+                    Type(np.uint8),
+                    Save(
+                        [f"output/dem/{region}.webp"],
+                        quality=actual_quality,
+                        lossless=False,
+                    ),
+                )
+
+                coefficients = results[0].get("coefficients", [{}])[0]
+                a = coefficients.get("a", 1.0)
+                b = coefficients.get("b", 0.0)
+                compression_factors.append(
+                    {
+                        "filename": f"{region}.webp",
+                        "a": float(a),
+                        "b": float(b),
+                        "latitude_start": float(latitude),
+                        "longitude_start": float(longitude),
+                        "latitude_end": float(end_latitude),
+                        "longitude_end": float(end_longitude),
+                        "width": int(image_size[0]),
+                        "height": int(image_size[1]),
+                    }
+                )
             else:
+                # 16-bit compression
+                min_val = np.min(image)
                 a = 0.25
-                b = -compression.get_min_max(['images/dem_no_oceans.tif'])[0]
-                compression_factors.append({
-                    "filename": f'{region}.webp',
-                    "a": float(a),
-                    "b": float(b),
-                    "latitude_start": float(latitude),
-                    "longitude_start": float(longitude),
-                    "latitude_end": float(end_latitude),
-                    "longitude_end": float(end_longitude),
-                    "width": int(image_size[0]),
-                    "height": int(image_size[1])
-                })
-                compression.split_16_bits(
-                    'images/dem_no_oceans.tif', 'images/dem_lower.tif', 'images/dem_upper.tif', a, b)
-                compression.minify_multiple(['images/dem_lower.tif', 'images/dem_upper.tif'], None, None,
-                                            f'dem-{region}', True, 100, True, should_print=False, a_override=1, b_override=0)
-                os.rename(f'output/dem-{region}-1-3.webp',
-                          f'output/dem/{region}.webp')
+                b = -min_val
+                _, results = process(
+                    [image],
+                    LinearCompression(a, b),
+                    Type(np.uint16),
+                    Split16Bits(),
+                    Group(2),
+                    Save(
+                        lambda i: f"output/dem/{region}.webp",
+                        quality=100,
+                        lossless=True,
+                    ),
+                )
+                compression_factors.append(
+                    {
+                        "filename": f"{region}.webp",
+                        "a": float(a),
+                        "b": float(b),
+                        "latitude_start": float(latitude),
+                        "longitude_start": float(longitude),
+                        "latitude_end": float(end_latitude),
+                        "longitude_end": float(end_longitude),
+                        "width": int(image_size[0]),
+                        "height": int(image_size[1]),
+                    }
+                )
             pbar.update(1)
 
-    with open('output/dem/index.json', 'w') as f:
-        index = json.dumps({
-            "compression_method": "8-bit" if compress_images else "16-bit",
-            "version": current_version,
-            "resolution_arc_seconds": true_resolution,
-            "files": compression_factors
-        })
-        index = re.sub(r'\s', '', index)
+    with open("output/dem/index.json", "w") as f:
+        index = json.dumps(
+            {
+                "compression_method": "8-bit" if compress_images else "16-bit",
+                "version": current_version,
+                "resolution_arc_seconds": true_resolution,
+                "files": compression_factors,
+            }
+        )
+        index = re.sub(r"\s", "", index)
         f.write(index)
 
     # Compress the contents of the dem directory to a zip file
-    with zipfile.ZipFile(f'output/dem-{current_version}.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
-        for root, dirs, files in os.walk('output/dem'):
+    with zipfile.ZipFile(
+        f"output/dem-{current_version}.zip", "w", zipfile.ZIP_DEFLATED
+    ) as zipf:
+        for root, dirs, files in os.walk("output/dem"):
             for file in files:
-                zipf.write(os.path.join(root, file), os.path.relpath(
-                    os.path.join(root, file), 'output/dem'))
+                zipf.write(
+                    os.path.join(root, file),
+                    os.path.relpath(os.path.join(root, file), "output/dem"),
+                )
--- scripts/operators/basic.py
+++ scripts/operators/basic.py
@@ -53,14 +53,22 @@ class ReplaceInvalid(ImageOperator):
 
 
 class Replace(ImageOperator):
-    def __init__(self, target_value, replacement=0):
-        self.target_value = target_value
+    def __init__(self, target_values, replacement=0, inverse=False):
+        self.target_values = (
+            target_values
+            if isinstance(target_values, (list, tuple))
+            else [target_values]
+        )
         self.replacement = replacement
+        self.inverse = inverse
 
     def apply(self, images):
         output = []
         for image in images:
-            output.append(np.where(image == self.target_value, self.replacement, image))
+            mask = np.isin(image, self.target_values)
+            if self.inverse:
+                mask = ~mask
+            output.append(np.where(mask, self.replacement, image))
         return output, {}
 
 
@@ -422,3 +430,32 @@ class Min(ImageOperator):
     def apply(self, images):
         combined = np.min(np.array(images), axis=0)
         return [combined], {}
+
+
+class Conditional(ImageOperator):
+    def __init__(self, condition, operator_if_true, operator_if_false=None):
+        self.condition = condition
+        self.operator_if_true = operator_if_true
+        self.operator_if_false = operator_if_false
+
+    def apply(self, images):
+        output = []
+        metadata = []
+        for image in images:
+            evaluated_condition = (
+                self.condition
+                if not callable(self.condition)
+                else self.condition(image)
+            )
+
+            operation = (
+                self.operator_if_true if evaluated_condition else self.operator_if_false
+            )
+            if operation is None:
+                output.append(image)
+                continue
+            result, meta = operation.apply([image])
+            metadata.append(meta)
+            for i in result:
+                output.append(i)
+        return output, {"metadata": metadata}
