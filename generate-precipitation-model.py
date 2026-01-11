2d26c396cd3ee2b9b6b303d684dced89ecb98f4e
Kyle Corry <kylecorry31@gmail.com>
2026-01-11 12:14:23 -0500

    refactor: use operations in precipitaiton model

--- generate-precipitation-model.py
+++ generate-precipitation-model.py
@@ -1,59 +1,44 @@
-from scripts import era5, compression, natural_earth, progress
-import os
+from scripts import era5, natural_earth
+from scripts.operators import (
+    process,
+    Split16Bits,
+    Save,
+    Resize,
+    RemoveOceans,
+    Type,
+    Map,
+    LinearCompression,
+)
+import numpy as np
 
 # Follow these instructions to sign in: https://cds.climate.copernicus.eu/how-to-api#install-the-cds-api-client
 
 start_year = 1991
 end_year = 2020
-remove_oceans = True
 
-era5.download(start_year, end_year)
-natural_earth.download()
-era5.process_precipitation(start_year, end_year)
-
-files = [
-    f'images/{start_year}-{end_year}-{month}-total_precipitation.tif' for month in range(1, 13)]
-
-# Zero out the oceans of each file
-if remove_oceans:
-    with progress.progress('Removing oceans', len(files)) as pbar:
-        for file in files:
-            natural_earth.remove_oceans_from_tif(file, file.replace(
-                'total_precipitation', 'total_precipitation_land'))
-            pbar.update(1)
+# era5.download(start_year, end_year)
+# natural_earth.download()
+# era5.process_precipitation(start_year, end_year)
 
 files = [
-    f'images/{start_year}-{end_year}-{month}-{'total_precipitation_land' if remove_oceans else 'total_precipitation'}.tif' for month in range(1, 13)]
+    f"images/{start_year}-{end_year}-{month}-total_precipitation.tif"
+    for month in range(1, 13)
+]
 
-
-new_files = []
-with progress.progress('Splitting images', len(files)) as pbar:
-    for file in files:
-        # Split the 16-bit image into 2 8-bit images
-        compression.split_16_bits(file, file.replace('.tif', '_lower.tif'),
-                      file.replace('.tif', '_upper.tif'), 1000 * 30)
-        new_files.append(file.replace('.tif', '_lower.tif'))
-        new_files.append(file.replace('.tif', '_upper.tif'))
-        pbar.update(1)
-
-compression.minify_multiple(
-    new_files,
-    lambda x: x,
-    -999,
-    'precipitation',
-    True,
-    100,
-    True,
-    (360, 180),
-    grouping=2,
-    a_override=1,
-    b_override=0
+compression_index = 1
+_, metadata = process(
+    files,
+    RemoveOceans(),
+    LinearCompression(1000 * 30, 0),
+    Map(lambda image: np.rint(image)),
+    Resize((360, 180), exact=True),
+    Type(np.uint16),
+    Split16Bits(),
+    Save(
+        lambda i: f"output/precipitation/precipitation-{i+1}.webp",
+        quality=100,
+        lossless=True,
+    ),
+    show_progress=True,
 )
-
-# Rename to be the month numbers (instead of 2 * m-2*m + 1)
-for i in range(12):
-    first = i * 2 + 1
-    second = first + 1
-    file_name = f'output/precipitation-{first}-{second}.webp'
-    new_file_name = f'output/precipitation-{i + 1}.webp'
-    os.rename(file_name, new_file_name)
+print(metadata[compression_index])
--- scripts/operators/resize.py
+++ scripts/operators/resize.py
@@ -4,14 +4,18 @@ import numpy as np
 
 
 class Resize(ImageOperator):
-    def __init__(self, max_size, resample=Image.NEAREST):
+    def __init__(self, max_size, resample=Image.NEAREST, exact=False):
         self.max_size = max_size
         self.resample = resample
+        self.exact = exact
 
     def apply(self, images):
         output = []
         for image in images:
             img = Image.fromarray(image)
-            img.thumbnail(self.max_size, resample=self.resample)
+            if self.exact:
+                img = img.resize(self.max_size, self.resample)
+            else:
+                img.thumbnail(self.max_size, resample=self.resample)
             output.append(np.array(img))
         return output, {}
